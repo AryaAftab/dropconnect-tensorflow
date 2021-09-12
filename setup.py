d89c47f13832e10767901ec2ef6d214e86f21874
Arya Aftab <arya.aftab@gmail.com>
2021-09-12 08:51:59 +0430

    Added Description to Pypi package!

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
+import os
+import codecs
 from setuptools import setup, find_packages
+
+
+current_path = os.path.abspath(os.path.dirname(__file__))
+
+
+def read_file(*parts):
+    with codecs.open(os.path.join(current_path, *parts), 'r', 'utf8') as reader:
+        return reader.read()
+
+
 
 setup(
   name = 'dropconnect-tensorflow',
@@ -6,6 +18,8 @@ setup(
   version = '0.1.0',
   license='MIT',
   description = 'Drop Connect - Tensorflow',
+  long_description=read_file('README.md'),
+  long_description_content_type='text/markdown',
   author = 'Arya Aftab',
   author_email = 'arya.aftab@gmail.com',
   url = 'https://github.com/AryaAftab/dropconnect-tensorflow',
